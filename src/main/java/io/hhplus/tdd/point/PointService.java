760399af426d5a2fee42391524eb72e73deab1be
이주영 <141599648+ljy1348@users.noreply.github.com>
2024-09-26 22:25:06 +0900

    feat : 포인트 서비스에 lock 설정

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @RequiredArgsConstructor
@@ -16,43 +19,59 @@ public class PointService {
 
     private final long MAX_POINT = PointPolicy.MAX_POINT;
 
-//    포인트 조회
+    private final ConcurrentHashMap<Long, Lock> locks = new ConcurrentHashMap<>();
+
+    //    포인트 조회
     public UserPoint lookUp(long id) {
         return userPointTable.selectById(id);
     }
 
-//    포인트 사용
+    //    포인트 사용
     public UserPoint use(long id, long amount) {
-        UserPoint selectUser = userPointTable.selectById(id);
-        long originPoint = selectUser.point();
-        long newPoint = originPoint - amount;
-        if (originPoint < amount) {
-            throw new IllegalArgumentException("포인트가 부족합니다.");
+        Lock lock = locks.computeIfAbsent(id, key -> new ReentrantLock());
+        lock.lock();
+        try {
+            UserPoint selectUser = userPointTable.selectById(id);
+            long originPoint = selectUser.point();
+            long newPoint = originPoint - amount;
+            if (originPoint < amount) {
+                throw new IllegalArgumentException("포인트가 부족합니다.");
+            }
+            UserPoint updatedUser = userPointTable.insertOrUpdate(id, newPoint);
+            historyInsert(updatedUser.id(), updatedUser.point(), TransactionType.USE, System.currentTimeMillis());
+            return updatedUser;
+        } finally {
+            lock.unlock();
         }
-        UserPoint updatedUser = userPointTable.insertOrUpdate(id, newPoint);
-        historyInsert(updatedUser.id(), updatedUser.point(), TransactionType.USE, System.currentTimeMillis());
-        return updatedUser;
+
     }
 
-//    포인트 내역 조회
+    //    포인트 내역 조회
     public List<PointHistory> historyLookup(long id) {
         return pointHistoryTable.selectAllByUserId(id);
     }
 
-//    포인트 충전
+    //    포인트 충전
     public UserPoint charge(long id, long amount) {
-        UserPoint selectUser = userPointTable.selectById(id);
-        long originPoint = selectUser.point();
-        long newPoint = originPoint + amount;
-        if (newPoint > MAX_POINT) {
-            throw new IllegalArgumentException("포인트 최대 한도를 초과할 수 없습니다.");
+        Lock lock = locks.computeIfAbsent(id, key -> new ReentrantLock());
+        lock.lock();
+        try {
+            UserPoint selectUser = userPointTable.selectById(id);
+            long originPoint = selectUser.point();
+            long newPoint = originPoint + amount;
+            if (newPoint > MAX_POINT) {
+                throw new IllegalArgumentException("포인트 최대 한도를 초과할 수 없습니다.");
+            }
+            UserPoint updatedUser = userPointTable.insertOrUpdate(id, newPoint);
+            historyInsert(updatedUser.id(), updatedUser.point(), TransactionType.CHARGE, System.currentTimeMillis());
+            return updatedUser;
+        } finally {
+            lock.unlock();
         }
-        UserPoint updatedUser = userPointTable.insertOrUpdate(id, newPoint);
-        historyInsert(updatedUser.id(), updatedUser.point(), TransactionType.CHARGE, System.currentTimeMillis());
-        return updatedUser;
+
     }
 
-//    포인트 내역 추가
+    //    포인트 내역 추가
     private PointHistory historyInsert(long userId, long amount, TransactionType type,  long point) {
         return pointHistoryTable.insert(userId, amount, type, point);
     }
