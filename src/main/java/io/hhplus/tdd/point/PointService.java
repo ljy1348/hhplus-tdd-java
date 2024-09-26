213e9488f1607a7acdfa48a8d5942ef874451065
이주영 <141599648+ljy1348@users.noreply.github.com>
2024-09-26 11:00:54 +0900

    feat : 포인트 서비스 클래스 추가

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,17 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PointService {
+
+//    di
+    private final PointHistoryTable pointHistoryTable;
+    private final UserPointTable userPointTable;
+
+    
+}
