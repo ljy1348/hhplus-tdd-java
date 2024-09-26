1514fd55c2bd080d20b5fa8424acf5d827f5aa5e
이주영 <141599648+ljy1348@users.noreply.github.com>
2024-09-26 11:33:13 +0900

    feat : 포인트 서비스 기본 기능 구현

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -5,13 +5,56 @@ import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PointService {
 
-//    di
     private final PointHistoryTable pointHistoryTable;
     private final UserPointTable userPointTable;
 
-    
+    private final long MAX_POINT = 100000L;
+
+//    포인트 조회
+    public UserPoint lookUp(long id) {
+        return userPointTable.selectById(id);
+    }
+
+//    포인트 사용
+    public UserPoint use(long id, long point) {
+        UserPoint selectUser = userPointTable.selectById(id);
+        long originPoint = selectUser.point();
+        long newPoint = originPoint - point;
+        if (originPoint < point) {
+            throw new IllegalArgumentException("포인트가 부족합니다.");
+        }
+        UserPoint updatedUser = userPointTable.insertOrUpdate(id, newPoint);
+        historyInsert(updatedUser.id(), updatedUser.point(), TransactionType.USE, point);
+        return updatedUser;
+    }
+
+//    포인트 내역 조회
+    public List<PointHistory> historyLookup(long id) {
+        return pointHistoryTable.selectAllByUserId(id);
+    }
+
+//    포인트 충전
+    public UserPoint charge(long id, long point) {
+        UserPoint selectUser = userPointTable.selectById(id);
+        long originPoint = selectUser.point();
+        long newPoint = originPoint + point;
+        if (newPoint > MAX_POINT) {
+            throw new IllegalArgumentException("포인트 최대 한도를 초과할 수 없습니다.");
+        }
+        UserPoint updatedUser = userPointTable.insertOrUpdate(id, newPoint);
+        historyInsert(updatedUser.id(), updatedUser.point(), TransactionType.CHARGE, point);
+        return updatedUser;
+    }
+
+//    포인트 내역 추가
+    private PointHistory historyInsert(long userId, long amount, TransactionType type,  long point) {
+        return pointHistoryTable.insert(userId, amount, type, point);
+    }
+
 }
